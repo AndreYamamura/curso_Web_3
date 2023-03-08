71cc2874b66f5ec868f169dd05511d97a51b7369
AndreYamamura <38484286+AndreYamamura@users.noreply.github.com>
2023-03-08 20:32:09 -0300

    Delete Exercicios directory

--- Exercicios/migrations/1_CursoWeb3_Exercicio1.js
+++
@@ -1,5 +0,0 @@
-const CursoWeb3_Exercicio1 = artifacts.require("CursoWeb3_Exercicio1");
-
-module.exports = function (deployer) {
-  deployer.deploy(CursoWeb3_Exercicio1);
-};
--- Exercicios/migrations/2_CursoWeb3_Exercicio2.js
+++
@@ -1,5 +0,0 @@
-const CursoWeb3_Exercicio2 = artifacts.require("CursoWeb3_Exercicio2");
-
-module.exports = function (deployer) {
-  deployer.deploy(CursoWeb3_Exercicio2, "Andre");
-};
